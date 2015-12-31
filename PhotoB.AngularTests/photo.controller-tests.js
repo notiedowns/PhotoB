78ce05e6dd7f58905a5a10d83584a7e3563697ae
Wayne <notiedowns@yahoo.co.nz>
2015-12-31 11:52:31 +0100

    Tried a jasmine test - not working. Added 'E' directive. Added cookie for stylesheet

---
+++ PhotoB.AngularTests/photo.controller-tests.js
@@ -0,0 +1,27 @@
+﻿'use strict';
+
+(function () {
+
+    describe('During construction of the controller', function () {
+
+        var scope, controller, photoRepositoryMock, photos;
+
+        beforeEach(function () {
+            module('shopModule');
+
+            inject(function ($rootScope, $controller, photoRepository) {
+                scope = $rootScope.$new();
+                photoRepositoryMock = sinon.stub(photoRepository);
+                photos = [{ foo: 'bar' }];
+                photoRepositoryMock.getPhotos.returns(photos);
+                controller = $controller('PhotolistController', { $scope: scope });
+            });   
+        });
+
+        it('Should set the photos from the repository', function () {
+            expect(scope.photos).toBe(photos);
+        });
+
+    });
+
+})();
--- PhotoB/Scripts/Shop/shop-module.js
+++ PhotoB/Scripts/Shop/shop-module.js
@@ -5,15 +5,27 @@
     shopModule.config(function ($routeProvider) {
 
         $routeProvider
+            .when("/Start", {
+                templateUrl: "/templates/photo/photo-list.html",
+                controller: "PhotoCreateController"
+            })
             .when("/PhotoList", {
-                templateUrl: "/templates/photoListDetails.html",
+                templateUrl: "/templates/photo/photo-list.html",
                 controller: "PhotolistController"
             })
             .when("/CreatePhoto", {
-                templateUrl: "/templates/createPhotoDetails.html",
+                templateUrl: "/templates/photo/photo-create.html",
                 controller: "PhotoCreateController"
             })
         .otherwise({ redirectTo: "/Start" });
+    });
+
+
+    shopModule.directive('sideNavbar', function () {
+        return {
+            restrict: 'E',
+            templateUrl: '/Templates/nav/side-navbar.html'
+        };
     });
 
 })();
