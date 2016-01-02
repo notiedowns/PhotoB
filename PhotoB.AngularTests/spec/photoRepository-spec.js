99777cc0ae245d183c901aacd274ed9e8bc2d5a4
Wayne <notiedowns@yahoo.co.nz>
2016-01-02 17:36:55 +0100

    Http tests with promises working

---
+++ PhotoB.AngularTests/spec/photoRepository-spec.js
@@ -0,0 +1,36 @@
+﻿describe('get photos', function () {
+    var photoRepository = {};
+    var $httpBackend;
+
+    beforeEach(function () {
+        // module and inject don't need to be qualified with 'angular.mock' if you want. 
+        angular.mock.module('shopModule');
+
+        angular.mock.inject(function (_photoRepository_, _$httpBackend_) {
+            photoRepository = _photoRepository_;
+            $httpBackend = _$httpBackend_;
+        });
+    });
+
+    
+
+    it('should retrieve a list of photos', function () {
+        var testPhotoData = { foo: 'bar' }; 
+        console.log(angular.mock.dump(testPhotoData));
+
+        $httpBackend.when('GET', '/Photo/GetPhotos').respond(200, testPhotoData);
+
+        var responseData;
+        photoRepository.getPhotos().then(function (data) {
+            responseData = data;
+        });
+
+        $httpBackend.flush();
+
+        expect(responseData).toEqual(testPhotoData);
+    });
+    
+    it('should create new photo', function () {
+        expect(photoRepository.createPhoto()).toEqual(true);
+    });
+});
