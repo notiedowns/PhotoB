acc55132b4e49898e8be866df29dd09e5ee43d04
Wayne <notiedowns@yahoo.co.nz>
2016-02-06 21:27:44 +0100

    Set sandstone as default

--- PhotoB/App_Start/BundleConfig.cs
+++ PhotoB/App_Start/BundleConfig.cs
@@ -29,7 +29,7 @@ namespace PhotoB
                       "~/Scripts/angular-route.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-sandstone.css",
                       "~/Scripts/Bootstrap-table/bootstrap-table.css"));
 
             bundles.Add(new StyleBundle("~/Scripts/Bootstrap-table").Include(
--- PhotoB/Controllers/HomeController.cs
+++ PhotoB/Controllers/HomeController.cs
@@ -9,11 +9,6 @@ namespace PhotoB.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
-        {
-            return View();
-        }
-
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
